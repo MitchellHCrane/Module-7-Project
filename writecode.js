35d85abb8561ffc8775eabefddcac355f87d2477
Mitchell Crane <mitchellhcrane@gmail.com>
2021-07-23 20:49:22 -0700

    fixed step 3

--- writecode.js
+++ writecode.js
@@ -1,11 +1,17 @@
-function addToZero([num1, num2]) {
-  const sum = num1 + num2;
-  if ((sum = 0)) {
-    return true;
-  } else {
-    return false;
+function addToZero(arr) {
+  // console.log(arr)
+  let sum = 0;
+  // console.log(sum)
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    // console.log(sum)
   }
-  console.log(addToZero(3, -3));
+  if (sum === 0) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 }
 
+addToZero([2, -2]);
 //Runtime of this code: O(n)
